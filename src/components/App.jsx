33e56decbd4c0a991c86f379ccf2a1650e49a938
zfedorovska <43138422+zfedorovska@users.noreply.github.com>
2022-04-23 21:31:19 +0300

    added save contacts to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,7 @@ import s from '../components/PhoneBook/PhoneBook.module.css';
 
 export default class App extends Component {
   state = {
-    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }],
+    contacts: [],
     filter: ''
   }
 
@@ -47,6 +44,22 @@ export default class App extends Component {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+       this.setState({ contacts: parsedContacts });   
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+      if (this.state.contacts !== prevState.contacts) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
+    }
+
 
   render() {
     const { filter } = this.state;
